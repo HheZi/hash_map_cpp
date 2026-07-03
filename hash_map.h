dab2558c446cfaa887bf744ba0f27e860aa81c9e
Pavel Tseva <paveltseva@gmail.com>
2026-07-03 23:06:15 +0300

    Make project header only

--- hash_map.h
+++ hash_map.h
@@ -1,9 +1,38 @@
+#pragma once
+
 #include <cstddef>
 #include <stdexcept>
 #include <vector>
-#include "include/assignable_and_hashable.h"
-#include "include/node.h"
-#include "include/hash_map.h"
+#include "assignable_and_hashable.h"
+#include "node.h"
+#include "hash_map.h"
+
+template <EquableAndHashable K, typename V>
+class HashMap {
+private:
+  constexpr static size_t DEFAULT_CAPACITY = 20;
+  size_t capacity{}, size{};
+  float load_factor{0.75};
+  std::vector<Node<K, V> *> table;
+
+  size_t getHashKey(const K &key, const size_t capacity) const;
+  bool shouldReziseTable();
+  void resizeTable(const size_t new_capacity);
+
+public:
+  HashMap();
+  HashMap(size_t capacity);
+  HashMap(float load_factor);
+  ~HashMap();
+
+  V *get(const K &key) const;
+  void put(K key, V value);
+  void remove(const K &key);
+  size_t getSize() const {return size;}
+  size_t getCapacity() const {return capacity;}
+
+  V *operator[](const K &key) const;
+};
 
 template <EquableAndHashable K, typename V>
 HashMap<K, V>::HashMap()
@@ -138,3 +167,4 @@ template <EquableAndHashable K, typename V>
 V *HashMap<K, V>::operator[](const K &key) const {
   return get(key);
 }
+
